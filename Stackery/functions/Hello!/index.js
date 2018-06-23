ff48764477e5bc9cd9ab88719338136c66d9804e
Toby Fee <TobyFee@gmail.com>
2018-06-22 22:01:36 -0700

    random array element

--- Stackery/functions/Hello!/index.js
+++ Stackery/functions/Hello!/index.js
@@ -3,7 +3,8 @@ var rp = require('request-promise-native');
 module.exports = async request => {  
   let res = await rp('https://s3.amazonaws.com/unix-fortune/fortunes.txt')
   res.responseType = ('text')
-  let responseMessage = res.split('%').join(`<p>`)
+  let responseArray = res.split('%')
+  let responseMessage = responseArray[Math.floor(Math.random()*responseArray.length)];
   let response = {
     statusCode: 200,
     headers: {
