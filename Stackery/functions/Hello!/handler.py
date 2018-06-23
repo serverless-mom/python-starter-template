63ec35f6271716b8f79c2aad3d677ad844a12844
Toby Fee <TobyFee@gmail.com>
2018-06-23 14:14:33 -0700

    syntax error

--- Stackery/functions/Hello!/handler.py
+++ Stackery/functions/Hello!/handler.py
@@ -2,8 +2,10 @@ def handler(message, context):
     print(message)
 
     return {
-    "Content-Type": "text/html"
-
+    "Content-Type": "text/html",
     "body": "<h1>Hello, the current time is 15:40:19.009371</h1>",
     "statusCode": 200
 }
+
+
+handler('hello','david')
