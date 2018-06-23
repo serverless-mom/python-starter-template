82a5a2c17d84499a9484c1def43bae6e4f17468d
Toby Fee <TobyFee@gmail.com>
2018-06-23 13:30:45 -0700

    first commit

--- Stackery/functions/Hello!/handler.py
+++ Stackery/functions/Hello!/handler.py
@@ -1,4 +1,9 @@
 def handler(message, context):
     print(message)
 
-    return {}
+    return {
+    "Content-Type": "text/html"
+
+    "body": "<h1>Hello, the current time is 15:40:19.009371</h1>",
+    "statusCode": 200
+}
