a1136b1c03c83cd3203577ffa45c1af8e2cf6171
Toby Fee <TobyFee@gmail.com>
2018-06-22 21:25:09 -0700

    async await

--- Stackery/functions/Hello!/index.js
+++ Stackery/functions/Hello!/index.js
@@ -1,46 +1,13 @@
-var http = require('http')
-let fullText = ''
-module.exports = async request => {  return http.get('http://s3.amazonaws.com/unix-fortune/fortunes.txt', (res) => {
-  const { statusCode } = res;
+var rp = require('request-promise');
 
-  let error;
-  if (statusCode !== 200) {
-    error = new Error('Request Failed.\n' +
-                      `Status Code: ${statusCode}`);
-  }
-  if (error) {
-    //console.error(error.message);
-    // consume response data to free up memory
-    res.resume();
-    return;
-  }
-
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-       fullText = rawData;
-    } catch (e) {
-      console.error(e.message);
-    }
-    let fullTextArray = fullText.toString('utf8').split('%');
-    let responseLine = fullTextArray[Math.floor(Math.random()*fullTextArray.length)];
-    //console.log(responseLine);
-
-      // Build an HTTP response.
-    let response = {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html"
-      },
-      body: `<h2>${responseLine}</h2>`
-    };
-    return response;
-
-  
-  });
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-});
+module.exports = async request => {  
+  let responseMessage = await rp('http://www.google.com')
+  let response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body: `<h2>${responseMessage}</h2>`
+  };
+  return response;
 };
--- Stackery/functions/Hello!/test.js
+++ Stackery/functions/Hello!/test.js
@@ -1,50 +1,16 @@
-var http = require('http')
-let fullText = ''
+var rp = require('request-promise-native');
+
 
 var getData = async request =>{  // Log the request to the console.
-  return http.get('http://s3.amazonaws.com/unix-fortune/fortunes.txt', (res) => {
-  const { statusCode } = res;
-
-  let error;
-  if (statusCode !== 200) {
-    error = new Error('Request Failed.\n' +
-                      `Status Code: ${statusCode}`);
-  }
-  if (error) {
-    //console.error(error.message);
-    // consume response data to free up memory
-    res.resume();
-    return;
-  }
-
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-       fullText = rawData;
-    } catch (e) {
-      console.error(e.message);
-    }
-    let fullTextArray = fullText.toString('utf8').split('%');
-    let responseLine = fullTextArray[Math.floor(Math.random()*fullTextArray.length)];
-    //console.log(responseLine);
-
-      // Build an HTTP response.
+    let responseMessage = await rp('http://www.google.com')
     let response = {
       statusCode: 200,
       headers: {
         "Content-Type": "text/html"
       },
-      body: `<h2>${responseLine}</h2>`
+      body: `<h2>${responseMessage}</h2>`
     };
     return response;
-
-  
-  });
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-});
 }
 
 console.log(getData({}))
